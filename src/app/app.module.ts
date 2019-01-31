4672119fd81de3894baae4cd53cffe093e04e950
Kevin Norgaard <norgaark@uci.edu>
2019-01-30 21:00:17 -0800

    Added progress bar and updated styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GetstartedComponent } from './getstarted/getstarted.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
+import { SmallerHeaderComponent } from './smaller-header/smaller-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GetstartedComponent
+    GetstartedComponent,
+    ProgressBarComponent,
+    SmallerHeaderComponent
   ],
   imports: [
     BrowserModule,
